2647dc3b5cfbe5c3388b7cde2dbebba488e6bfd7
A.rahim <am43malik@gmail.com>
2022-12-19 18:33:41 +0530

    redux-store-aoi

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@
 import Cart from './components/Cart';
 import Produts from './components/Produts';
 import Home from './pages/Home';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';  
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';  
 import { Provider } from 'react-redux'
 import Navbara from './components/Navbar';
 import Store from './store/Store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 function App() {
+
+  let persistor = persistStore(Store);
   return (
     <div >
     
 
 
 <Provider store={Store}>
-<Router>
+  <PersistGate persistor={persistor}>
+  <Router>
   <Navbara/>
       <Routes>
       <Route path='/' element={<Home/>}/>
@@ -23,6 +28,8 @@ function App() {
     <Route path='/produts' element={<Produts/>}/>
       </Routes>
     </Router>
+  </PersistGate>
+
 </Provider>
 
   
--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cardReducer from './cartSlice'
 import ProductReducer from './productSlice'
-const Store= configureStore({
-    reducer:{
-        cart: cardReducer,
-        product:ProductReducer
+import storage from 'redux-persist/lib/storage'
+import {combineReducers} from "redux"; 
+import { persistReducer } from 'redux-persist'
 
-    }
+
+const persistConfig={
+     key:'root',
+    storage,
+}
+
+const reducer=combineReducers({
+    cart: cardReducer,
+    product:ProductReducer
+
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+
+const Store= configureStore({
+    reducer:persistedReducer
 })
 export default Store
